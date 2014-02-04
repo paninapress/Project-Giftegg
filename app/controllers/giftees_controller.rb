a8c8ff6266c0a1ce8b1cb520817b343bc9f0105a
paninapress <NPacifico25@gmail.com>
2014-02-03 17:39:14 -0800

    can now show who added giftee

--- app/controllers/giftees_controller.rb
+++ app/controllers/giftees_controller.rb
@@ -11,5 +11,8 @@ class GifteesController < ApplicationController
 
   def show
     @giftee = Giftee.find(params[:id])
+    #Show who added this giftee
+    users = User.all
+    @user = users.find(@giftee.user_id)
   end
 end
