013df6417ae910e1768488c77740acf0001223b9
paninapress <NPacifico25@gmail.com>
2014-02-03 16:56:19 -0800

    fixed birthday seed format

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,11 +13,10 @@ User.create(first_name: 'Janet', last_name: "Smith", email: 'janet@somemail.com'
 User.create(first_name: 'Jacob', last_name: "Smith", email: 'jacob@somemail.com')
 
 
-Giftee.create(first_name: 'John', last_name: "Smith", birthday: '04/25/2005', relation: 'cousin', user_id: '1')
-Giftee.create(first_name: 'Mark', last_name: "Smith", birthday: '04/25/2007', relation: 'cousin', user_id: '2')
-Giftee.create(first_name: 'Larry', last_name: "Smith", birthday: '04/25/2003', relation: 'cousin', user_id: '2')
-Giftee.create(first_name: 'Laura', last_name: "Smith", birthday: '04/25/2009', relation: 'cousin', user_id: '3')
-Giftee.create(first_name: 'Leslie', last_name: "Smith", birthday: '04/25/3009', relation: 'cousin', user_id: '3')
-Giftee.create(first_name: 'Apple', last_name: "Smith", birthday: '04/25/2003', relation: 'cousin', user_id: '4')
-Giftee.create(first_name: 'Fiona', last_name: "Smith", birthday: '04/25/2010', relation: 'cousin', user_id: '5')
-Giftee.create(first_name: 'North', last_name: "Smith", birthday: '04/25/2005', relation: 'cousin', user_id: '5') 
+Giftee.create(first_name: 'John', last_name: "Smith", birthday: '2005/04/25', relation: 'cousin', user_id: '1')
+Giftee.create(first_name: 'Mark', last_name: "Smith", birthday: '2007/04/25', relation: 'cousin', user_id: '2')
+Giftee.create(first_name: 'Larry', last_name: "Smith", birthday: '2008/04/25', relation: 'cousin', user_id: '2')
+Giftee.create(first_name: 'Leslie', last_name: "Smith", birthday: '2010/04/25', relation: 'cousin', user_id: '3')
+Giftee.create(first_name: 'Apple', last_name: "Smith", birthday: '2006/04/25', relation: 'cousin', user_id: '4')
+Giftee.create(first_name: 'Fiona', last_name: "Smith", birthday: '2003/04/25', relation: 'cousin', user_id: '5')
+Giftee.create(first_name: 'North', last_name: "Smith", birthday: '2002/04/25', relation: 'cousin', user_id: '5') 
