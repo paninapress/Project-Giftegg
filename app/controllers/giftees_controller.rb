ecc0cbdd742b537da2ceaf6236b25576e3bfd999
paninapress <NPacifico25@gmail.com>
2014-02-03 16:10:22 -0800

    added create giftee form

--- app/controllers/giftees_controller.rb
+++ app/controllers/giftees_controller.rb
@@ -1,7 +1,15 @@
 class GifteesController < ApplicationController
   def new
+    @giftee = Giftee.new
+  end
+
+  def create
+    new_giftee = params.require(:giftee).permit(:first_name, :last_name, :birthday, :relation, :notes)
+    giftee = User.create(new_giftee)
+    redirect_to user_path(giftee)
   end
 
   def show
+    @giftee = Giftee.find(params[:id])
   end
 end
