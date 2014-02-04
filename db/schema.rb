55926a4edcf0d625737aef5f9b2a8623731ffb24
paninapress <NPacifico25@gmail.com>
2014-02-04 12:24:45 -0800

    added remember token

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140204030934) do
+ActiveRecord::Schema.define(version: 20140204181521) do
 
   create_table "giftees", force: true do |t|
     t.string   "first_name"
@@ -35,6 +35,9 @@ ActiveRecord::Schema.define(version: 20140204030934) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
+    t.string   "remember_token"
   end
+
+  add_index "users", ["remember_token"], name: "index_users_on_remember_token"
 
 end
