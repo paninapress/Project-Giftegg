fba1653c70654aae9f6c69a4d941599aaed4aedb
paninapress <NPacifico25@gmail.com>
2014-02-04 16:42:14 -0800

    commented out user seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,11 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.create(first_name: 'Mary', last_name: "Smith", email: 'mary@somemail.com')
-User.create(first_name: 'Max', last_name: "Smith", email: 'max@somemail.com')
-User.create(first_name: 'Rodger', last_name: "Smith", email: 'rodger@somemail.com')
-User.create(first_name: 'Janet', last_name: "Smith", email: 'janet@somemail.com')
-User.create(first_name: 'Jacob', last_name: "Smith", email: 'jacob@somemail.com')
+# User.create(first_name: 'Mary', last_name: "Smith", email: 'mary@somemail.com')
+# User.create(first_name: 'Max', last_name: "Smith", email: 'max@somemail.com')
+# User.create(first_name: 'Rodger', last_name: "Smith", email: 'rodger@somemail.com')
+# User.create(first_name: 'Janet', last_name: "Smith", email: 'janet@somemail.com')
+# User.create(first_name: 'Jacob', last_name: "Smith", email: 'jacob@somemail.com')
 
 
 Giftee.create(first_name: 'John', last_name: "Smith", birthday: '2005/04/25', relation: 'cousin', user_id: '1')
